bb4ce400321aafbe2c3f61bcac3de4c9a7036a81
Anderson Silva <andssilva.23@gmail.com>
2021-10-28 11:15:18 -0400

    feat(req 5): :rocket: cria um endpoint para visualizar uma receita específica

--- src/routes/recipesRoute.js
+++ src/routes/recipesRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const verifyToken = require('../auth/verifyToken');
-const { createRecipe, getAllRecipes } = require('../controllers/recipesController');
+const { createRecipe, getAllRecipes, getRecipeById } = require('../controllers/recipesController');
 
 const router = express.Router();
 
 router.post('/', verifyToken, createRecipe);
 router.get('/', getAllRecipes);
+router.get('/:id', getRecipeById);
 
 module.exports = router;
