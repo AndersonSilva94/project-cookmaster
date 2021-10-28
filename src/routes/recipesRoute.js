aa476eb54b465ee6a93d8424398873b685dabe1d
Anderson Silva <andssilva.23@gmail.com>
2021-10-28 12:40:46 -0400

    feat(req 9): :rocket: cria um endpoint para a adição de uma imagem a uma receita

--- src/routes/recipesRoute.js
+++ src/routes/recipesRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const verifyToken = require('../auth/verifyToken');
 const { createRecipe, getAllRecipes,
   getRecipeById, editRecipe, deleteRecipe } = require('../controllers/recipesController');
+const { uploadImg } = require('../controllers/uploadsController');
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get('/', getAllRecipes);
 router.get('/:id', getRecipeById);
 router.put('/:id', verifyToken, editRecipe);
 router.delete('/:id', verifyToken, deleteRecipe);
+router.put('/:id/image', verifyToken, uploadImg);
 
 module.exports = router;
