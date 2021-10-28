ff39d6a344acf95c0f726ff423ea84541ac75eb3
Anderson Silva <andssilva.23@gmail.com>
2021-10-28 01:16:55 -0400

    feat(req 1): :rocket: cria um endpoint para o cadastro de usuários

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const usersRoutes = require('../routes/usersRoute');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', usersRoutes);
+
+// middleware para tratamento de erro genérico
+app.use((err, _req, res, _next) => {
+  if (err.status) return res.status(err.status).json({ message: err.message });
+  return res.status(500).json({ message: err.message });
+});
 
 module.exports = app;
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,6 +3,7 @@ const { create } = require('../services/usersService');
 const createUser = async (request, response, next) => {
   try {
     const { name, email, password, role = 'user' } = request.body;
+    // console.log(request.body);
     const validyUser = await create(name, email, password, role);
 
     return response.status(validyUser.status).json(validyUser.message);
