d019ac606b24cfde11f3956fee70469625c2f9b8
Anderson Silva <andssilva.23@gmail.com>
2021-10-28 12:17:45 -0400

    feat: :rocket: cria a rota para edição de uma receita

--- src/routes/recipesRoute.js
+++ src/routes/recipesRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const verifyToken = require('../auth/verifyToken');
-const { createRecipe, getAllRecipes, getRecipeById } = require('../controllers/recipesController');
+const { createRecipe, getAllRecipes,
+  getRecipeById, editRecipe } = require('../controllers/recipesController');
 
 const router = express.Router();
 
 router.post('/', verifyToken, createRecipe);
 router.get('/', getAllRecipes);
 router.get('/:id', getRecipeById);
+router.put('/:id', verifyToken, editRecipe);
 
 module.exports = router;
