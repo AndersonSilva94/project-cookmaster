80dd88dbca18a88b9f92f73cceede663dc0f72ff
Anderson Silva <andssilva.23@gmail.com>
2021-10-28 11:04:33 -0400

    feat(req 4): :rocket: cria um endpoint para a listagem de receitas

--- src/routes/recipesRoute.js
+++ src/routes/recipesRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const verifyToken = require('../auth/verifyToken');
-const { createRecipe } = require('../controllers/recipesController');
+const { createRecipe, getAllRecipes } = require('../controllers/recipesController');
 
 const router = express.Router();
 
 router.post('/', verifyToken, createRecipe);
+router.get('/', getAllRecipes);
 
 module.exports = router;
