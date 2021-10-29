55de4d48d0178d45450a93f34155d2140bab971c
Anderson Silva <andssilva.23@gmail.com>
2021-10-29 04:56:25 -0400

    feat: :rocket: cria os testes de recipes (parte 2) - GET recipes e GET recipes/:id

--- src/integration-tests/recipes.test.js
+++ src/integration-tests/recipes.test.js
@@ -111,6 +111,194 @@ describe('POST /recipes', () => {
     it('retorna o objeto "recipe"', () => {
       expect(response.body.recipe).to.have.all
         .keys(['name', 'ingredients', 'preparation', '_id', 'userId']);
-    })
-  })
-})
+    });
+  });
+});
+
+describe('GET /recipes', () => {
+  let connectionMock;
+
+  before(async () => {
+    connectionMock = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+  });
+
+  after(() => {
+    MongoClient.connect.restore();
+  });
+
+  describe('Valida que é possível listar as receitas sem estar autenticado', () => {
+    let response;
+
+    before(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.insertOne(userObj);
+
+      const { body: { token } } = await chai.request(server).post('/login').send(correctLogin);
+
+      await chai.request(server).post('/recipes').set('Authorization', token)
+        .send(recipeObj)
+
+      response = await chai.request(server).get('/recipes')
+    });
+
+    after(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.deleteOne({
+        email: 'andy@teste.com'
+      });
+    });
+
+    it('retorna status "200"', (done) => {
+      expect(response).to.have.status(200);
+      done();
+    });
+
+    // como verificar se é um array encontrado no link
+    // https://devhints.io/chai
+    it('retorna um array', (done) => {
+      expect(response.body).to.be.a('array');
+      done();
+    });
+  });
+
+  describe('Valida que é possível listar as receitas estando autenticado', () => {
+    let response;
+
+    before(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.insertOne(userObj);
+
+      const { body: { token } } = await chai.request(server).post('/login').send(correctLogin);
+
+      await chai.request(server).post('/recipes').set('Authorization', token)
+        .send(recipeObj)
+
+      response = await chai.request(server).get('/recipes').set('Authorization', token)
+    });
+
+    after(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.deleteOne({
+        email: 'andy@teste.com'
+      });
+    });
+
+    it('retorna status "200"', (done) => {
+      expect(response).to.have.status(200);
+      done();
+    });
+    it('retorna um array', (done) => {
+      expect(response.body).to.be.a('array');
+      done();
+    });
+  });
+});
+
+describe('GET /recipes/:id', () => {
+  let connectionMock;
+
+  before(async () => {
+    connectionMock = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+  });
+
+  after(() => {
+    MongoClient.connect.restore();
+  });
+
+  describe('Valida que é possível listar uma receita específica sem autenticação', () => {
+    let response;
+
+    before(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.insertOne(userObj);
+
+      const { body: { token } } = await chai.request(server).post('/login').send(correctLogin);
+
+      const { body: { recipe: { _id: idRecipe } } } = await chai.request(server)
+        .post('/recipes').set('Authorization', token).send(recipeObj)
+      // console.log(idRecipe)
+      response = await chai.request(server).get(`/recipes/${idRecipe}`)
+    });
+
+    after(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.deleteOne({
+        email: 'andy@teste.com'
+      });
+    });
+
+    it('retorna status "200"', (done) => {
+      expect(response).to.have.status(200);
+      done();
+    });
+    it('retorna as informações da receita', (done) => {
+      expect(response.body).to.have.all
+        .keys(['name', 'ingredients', 'preparation', '_id', 'userId']);
+      done();
+    });
+  });
+
+  describe('Valida que é possível listar uma receita específica com autenticação', () => {
+    let response;
+
+    before(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.insertOne(userObj);
+
+      const { body: { token } } = await chai.request(server).post('/login').send(correctLogin);
+
+      const { body: { recipe: { _id: idRecipe } } } = await chai.request(server)
+        .post('/recipes').set('Authorization', token).send(recipeObj)
+
+      response = await chai.request(server).get(`/recipes/${idRecipe}`).set('Authorization', token)
+    });
+
+    after(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.deleteOne({
+        email: 'andy@teste.com'
+      });
+    });
+
+    it('retorna status "200"', (done) => {
+      expect(response).to.have.status(200);
+      done();
+    });
+    it('retorna as informações da receita', (done) => {
+      expect(response.body).to.have.all
+        .keys(['name', 'ingredients', 'preparation', '_id', 'userId']);
+      done();
+    });
+  });
+
+  describe('Valida que não é possível listar uma receita que não existe', () => {
+    let response;
+
+    before(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.insertOne(userObj);
+
+      const { body: { token } } = await chai.request(server).post('/login').send(correctLogin);
+
+      response = await chai.request(server).get('/recipes/617bb4b35c0e605c3cc951rt')
+    });
+
+    after(async () => {
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.deleteOne({
+        email: 'andy@teste.com'
+      });
+    });
+
+    it('retorna status "404"', (done) => {
+      expect(response).to.have.status(404);
+      done();
+    });
+    it('"message" tem o valor "recipe not found"', (done) => {
+      expect(response.body.message).to.be.equals('recipe not found');
+      done();
+    });
+  });
+});
